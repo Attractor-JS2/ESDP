8557ef6f37b74502f56b1e38d1662da50aaa613c
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-03-20 19:03:34 +0600

    Добавил динамическое поле с датами приёмов

--- rekinetix-client/src/Containers/Course/CourseForm.jsx
+++ rekinetix-client/src/Containers/Course/CourseForm.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import DatePicker from 'react-datepicker';
 import PropTypes from 'prop-types';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 import FormInput from './components/FormInput';
 import FormTextarea from './components/FormTextarea';
 
@@ -22,7 +22,7 @@ class CourseForm extends Component {
       endDate: new Date(),
       courseAim: '',
       result: '',
-      attendancesSchedule: [],
+      attendancesSchedule: [{ attendanceDate: new Date() }],
       administratorName: '',
     },
   };
@@ -33,7 +33,7 @@ class CourseForm extends Component {
     this.setState((prevState) => ({
       course: { ...prevState.course, [name]: value },
     }));
-  }
+  };
 
   patientFieldChangeHandler = (event) => {
     const { target } = event;
@@ -47,20 +47,61 @@ class CourseForm extends Component {
         },
       },
     }));
-  }
+  };
 
   dateChangeHandler = (name) => (date) => {
     return this.setState((prevState) => ({
       course: {
         ...prevState.course,
         [name]: date,
-      }
-    }))
-  }
+      },
+    }));
+  };
+
+  attendancesChangeHandler = (index) => (date) => {
+    const course = { ...this.state.course };
+    const dates = [...course.attendancesSchedule];
+    dates[index].attendanceDate = date;
+    return this.setState((prevState) => ({
+      ...prevState,
+      course: {
+        ...prevState.course,
+        attendancesSchedule: [...dates],
+      },
+    }));
+  };
+
+  handleAddAttendance = () => {
+    const course = {...this.state.course};
+    const dates = [...course.attendancesSchedule, { attendanceDate: '' }]
+    // const newAttendance = { attendanceDate: '' };
+    this.setState((prevState) => ({
+      ...prevState,
+      course: {
+        ...prevState.course,
+        attendancesSchedule: [
+          ...dates,
+        ],
+      },
+    }));
+  };
+
+  handleRemoveAttendance = (index) => {
+    const course = { ...this.state.course };
+    const attendances = [...course.attendancesSchedule];
+    attendances.splice(index, 1);
+    this.setState((prevState) => ({
+      ...prevState,
+      course: {
+        ...prevState.course,
+        attendancesSchedule: [...attendances],
+      },
+    }));
+  };
 
   render() {
     return (
-      <div>
+      <div className="container">
         <form>
           <FormInput
             propertyName="title"
@@ -94,14 +135,18 @@ class CourseForm extends Component {
             handleChange={this.inputChangeHandler}
             placeholder=""
           />
-          <DatePicker
-            selected={this.state.course.startDate}
-            onChange={this.dateChangeHandler("startDate")}
-          />
-          <DatePicker
-            selected={this.state.course.endDate}
-            onChange={this.dateChangeHandler("endDate")}
-          />
+          <div>
+            <DatePicker
+              selected={this.state.course.startDate}
+              onChange={this.dateChangeHandler('startDate')}
+            />
+          </div>
+          <div>
+            <DatePicker
+              selected={this.state.course.endDate}
+              onChange={this.dateChangeHandler('endDate')}
+            />
+          </div>
           <FormInput
             propertyName="courseAim"
             inputType="text"
@@ -117,6 +162,37 @@ class CourseForm extends Component {
             rows={2}
             handleChange={this.inputChangeHandler}
           />
+
+          <div>
+            <h4>График прохождения курса</h4>
+            {this.state.course.attendancesSchedule.map(
+              ({ attendanceDate }, index) => {
+                return (
+                  <div key={index}>
+                    <DatePicker
+                      selected={attendanceDate}
+                      onChange={this.attendancesChangeHandler(index)}
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      onClick={() => this.handleRemoveAttendance(index)}
+                    >
+                      Удалить поле
+                    </button>
+                  </div>
+                );
+              },
+            )}
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.handleAddAttendance}
+            >
+              Добавить поле
+            </button>
+          </div>
+
           <FormInput
             propertyName="administratorName"
             inputType="text"
@@ -125,6 +201,7 @@ class CourseForm extends Component {
             handleChange={this.inputChangeHandler}
             placeholder=""
           />
+          <button type="submit" className="btn btn-primary">Сохранить форму</button>
         </form>
       </div>
     );
