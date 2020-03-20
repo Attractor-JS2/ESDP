f02a58236c163a87b7091e9dc071acf28ac0ce57
Гарик Абдуллаев <abdullayev.gv@gmail.com>
2020-03-20 12:27:25 +0600

    Добавил необходимые текстовые поля

--- rekinetix-client/src/Containers/Attendance/Attendance.js
+++ rekinetix-client/src/Containers/Attendance/Attendance.js
@@ -9,13 +9,15 @@ class Attendance extends Component {
       <Container>
         <h3>Приём</h3>
         <Formik 
-          initialValues={{fullName: ""}} 
+          initialValues={{patientName: "", medicName: "", patienthood: ""}} 
           onSubmit={data => {
           console.log(data);
         }}>
           {({}) => (
             <Form>
-              <Field placeholder="ФИО" name="fullName" type="input" as={Input}/>
+              <Field placeholder="ФИО пациента" name="patientName" type="input" as={Input}/>
+              <Field placeholder="ФИО принимающего специалиста" name="medicName" type="input" as={Input}/>
+              <Field placeholder="Состояние пациента после терапии" name="patienthood" type="input" as={Input}/>
               <Button type="submit">Сохранить</Button>
             </Form>
           )}
