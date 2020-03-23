e1f2e31a8a25ff9eaad90323d448e3c1169ce514
yelkanat <elkanat01@gmail.com>
2020-03-23 19:29:52 +0600

    Добавил в селекцонное меню значение по умалчанию
    добавил лейблы для форм, именил dateOfBirth  на Date()

--- rekinetix-client/src/Containers/PatientRecord/PatientRecord.js
+++ rekinetix-client/src/Containers/PatientRecord/PatientRecord.js
@@ -6,15 +6,15 @@ class PatientRecord extends Component {
 
     render() {
         return (
-            <Container className="mt-5">
+            <Container className="mb-5">
             <h3>Анкетные данные</h3>
             <Formik
         initialValues={{
                 patientName: "",
-                medicName: "",
+                medicName: "Мария Ивановна",
                 patientAddress: "",
-                dateOfBirth: "",
-                patientGender: "",
+                dateOfBirth: new Date(),
+                patientGender: "Мужчина",
                 patientAge: "",
                 patientContactPhone: "",
                 patientHeigt: "",
@@ -25,22 +25,22 @@ class PatientRecord extends Component {
         }}>
         {() => (
             <Form>
-            <Field  placeholder="ФИО принимающего специалиста" name="medicName" as="select">
+            <Field   label="ФИО принимающего специалиста" name="medicName" as="select">
             <option value="Мария Ивановна">Мария Ивановна</option>
         <option value="Генади Головкин">Генади Головкин</option>
         <option value="Евгени Петровичь">Евгени Петровичь</option>
         </Field>
-            <Field className="mt-2 mb-2" placeholder="ФИО пациента" name="patientName" type="input" as={Input}/>
-        <Field className="mb-2" placeholder="Адрес проживание пациента" name="patientAddress" type="input" as={Input}/>
-        <Field className="mb-2" placeholder="Дата рождение пациента" name="dateOfBirth" type="date" as={Input}/>
-        <Field  placeholder="Пол пациента" name="patientGender" as="select">
+            <Field className="mt-2 mb-2" label="ФИО принимающего специалиста"  placeholder="ФИО пациента" name="patientName" type="input" as={Input}/>
+        <Field className="mb-2" label="Адрес проживание пациента" placeholder="Адрес проживание пациента" name="patientAddress" type="input" as={Input}/>
+        <Field className="mb-2" label="Дата рождение пациента" placeholder="Дата рождение пациента" name="dateOfBirth" type="date" as={Input}/>
+        <Field label="Пол пациента" placeholder="Пол пациента" name="patientGender" as="select">
             <option value="Мужчина">Мужчина</option>
         <option value="Женчина">Женчина</option>
         </Field>
-        <Field className="mb-2" placeholder="Возрост пациента" name="patientAge" type="input" as={Input}/>
-        <Field className="mb-2" placeholder="Контактный телефон пациента" name="patientContactPhone" type="input" as={Input}/>
-        <Field className="mb-2" placeholder="Рост пациента" name="patientHeigt" type="input" as={Input}/>
-        <Field className="mb-2" placeholder="Вес пациента" name="patientWeight" type="input" as={Input}/>
+        <Field className="mb-2" label="Возрост пациента" placeholder="Возрост пациента" name="patientAge" type="input" as={Input}/>
+        <Field className="mb-2" label="Контактный телефон пациента" placeholder="Контактный телефон пациента" name="patientContactPhone" type="input" as={Input}/>
+        <Field className="mb-2" label="Рост пациента" placeholder="Рост пациента" name="patientHeigt" type="input" as={Input}/>
+        <Field className="mb-2" label="Вес пациента" placeholder="Вес пациента" name="patientWeight" type="input" as={Input}/>
         <Button type="submit">Сохранить</Button>
             </Form>
         )}
