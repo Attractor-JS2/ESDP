cca6476b009a15c4743c4719b39b14776467494f
yelkanat <elkanat01@gmail.com>
2020-03-23 13:59:46 +0600

    Создал флому
    создал форму для ФИО пациента, адрес проживание, дата рождение, пол пациента, возрост пациента, контактный телефон, рост и вес

--- rekinetix-client/src/Containers/PatientRecord/PatientRecord.js
+++ rekinetix-client/src/Containers/PatientRecord/PatientRecord.js
@@ -30,7 +30,17 @@ class PatientRecord extends Component {
         <option value="Генади Головкин">Генади Головкин</option>
         <option value="Евгени Петровичь">Евгени Петровичь</option>
         </Field>
-            
+            <Field className="mt-2 mb-2" placeholder="ФИО пациента" name="patientName" type="input" as={Input}/>
+        <Field className="mb-2" placeholder="Адрес проживание пациента" name="patientAddress" type="input" as={Input}/>
+        <Field className="mb-2" placeholder="Дата рождение пациента" name="dateOfBirth" type="date" as={Input}/>
+        <Field  placeholder="Пол пациента" name="patientGender" as="select">
+            <option value="Мужчина">Мужчина</option>
+        <option value="Женчина">Женчина</option>
+        </Field>
+        <Field className="mb-2" placeholder="Возрост пациента" name="patientAge" type="input" as={Input}/>
+        <Field className="mb-2" placeholder="Контактный телефон пациента" name="patientContactPhone" type="input" as={Input}/>
+        <Field className="mb-2" placeholder="Рост пациента" name="patientHeigt" type="input" as={Input}/>
+        <Field className="mb-2" placeholder="Вес пациента" name="patientWeight" type="input" as={Input}/>
             </Form>
         )}
     </Formik>
