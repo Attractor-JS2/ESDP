efc2230b75441f89dde0ed7f87a4afda71ac7dad
yelkanat <elkanat01@gmail.com>
2020-03-23 13:47:37 +0600

    создал класс PatientRecord

--- rekinetix-client/src/Containers/PatientRecord/PatientRecord.js
+++ rekinetix-client/src/Containers/PatientRecord/PatientRecord.js
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 import { Button, Input, Container} from "reactstrap";
 import { Formik, Field, Form } from "formik";
 
+class PatientRecord extends Component {
 
+    render() {
+        return (
+
+    );
+    }
+}
+
+export default PatientRecord;
 
 
 
