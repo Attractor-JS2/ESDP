b0ab7eebb08037dbd58a6093fae3c63380e1e5c4
Northern Stormy <gerrardthebanana@gmail.com>
2020-03-27 23:26:25 +0600

    #11 Создал роуты для компонента форм курса лечения и единичного приема

--- rekinetix-client/src/App.js
+++ rekinetix-client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import Attendance from "./Containers/Attendance/Attendance";
+import CourseForm from "./Containers/Course/CourseForm";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+        <Switch>
+          <Route path='/attendance' component={Attendance}/>
+          <Route path='/course' component={CourseForm} />
+        </Switch>
+    
   );
 }
 
