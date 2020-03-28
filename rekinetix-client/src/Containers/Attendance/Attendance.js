cdb1cd0d2fff171897ecb8210fea24f01b7226bc
Northern Stormy <gerrardthebanana@gmail.com>
2020-03-29 01:40:25 +0600

    #11 Отформатировал код, дабавил минимальное возможное значение 0 для обозначения количества процедур / медикаментов

--- rekinetix-client/src/Containers/Attendance/Attendance.js
+++ rekinetix-client/src/Containers/Attendance/Attendance.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import {Button, Input, Container} from "reactstrap";
-import { Formik, Field, FieldArray, Form } from "formik";
+import {Formik, Field, FieldArray, Form} from "formik";
 import DatePicker from 'react-date-picker';
 import {necessaryProcedures, units, availableProcedures} from './procedures'
 import {Persist} from "formik-persist";
@@ -13,21 +13,27 @@ class Attendance extends Component {
         <Formik
           initialValues={
             (JSON.parse(localStorage.getItem('attendance-form')) !== null) ? JSON.parse(localStorage.getItem('attendance-form')).values : (
-            {
-            date: new Date(),
-            patientName: "",
-            medicName: "",
-            manipulations: [{manipulation: {manipulationStage: '', manipulationName: '', manipulationAmount: 0, manipulationUnits: ''}}],
-            homeExcercising: [{excerciseName: ""}],
-            patienthood: "",
-            painScale: ""
-          })}
+              {
+                date: new Date(),
+                patientName: "",
+                medicName: "",
+                manipulations: [{
+                  manipulation: {
+                    manipulationStage: '',
+                    manipulationName: '',
+                    manipulationAmount: 0,
+                    manipulationUnits: ''
+                  }
+                }],
+                homeExcercising: [{excerciseName: ""}],
+                patienthood: "",
+                painScale: ""
+              })}
           onSubmit={(data, {resetForm}) => {
-          console.log(data);
+            console.log(data);
             localStorage.setItem('attendance-form', null);
-          resetForm()
-          
-        }}>
+            resetForm()
+          }}>
           {({values, setFieldValue, resetForm}) => (
             <Form>
               <DatePicker
@@ -35,69 +41,84 @@ class Attendance extends Component {
                 onChange={date => setFieldValue("date", date)}
               />
               <Field className="mt-2 mb-2" placeholder="ФИО пациента" name="patientName" type="input" as={Input}/>
-              <Field className="mb-2" placeholder="ФИО принимающего специалиста" name="medicName" type="input" as={Input}/>
+              <Field className="mb-2" placeholder="ФИО принимающего специалиста" name="medicName" type="input"
+                     as={Input}/>
               
               <FieldArray name="manipulations">
                 {arrayHelpers => (
                   <div className="mb-3 mt-3"> Процедуры и упражнения
                     {
                       values.manipulations.map((manipulation, index) => {
-                      return (
-                        <div key={index} style={values.manipulations[index].manipulation.isNew ? {backgroundColor: 'rgba(38, 150, 38, 0.62)'} : null} className='d-flex p-2 mb-1'>
-                          
-                          <Field
-                            name={`manipulations.${index}.manipulation.manipulationStage`}
-                            as={'select'}
-                          >
-                            <option value={''}>Выберите этап</option>
-                            {
-                              Object.keys(values.manipulations[index].manipulation.isNew ? availableProcedures : necessaryProcedures).map((stage, i) => {
-                              return <option key={i} value={stage}>{stage}</option>
-                            })}
-                          </Field>
-                          
-                          <Field
-                            className="ml-3"
-                            name={`manipulations.${index}.manipulation.manipulationName`}
-                            as={'select'}
-                          >
-                            <option value={''}>Выберите манипуляцию</option>
-                            {
-                              values.manipulations[index].manipulation.manipulationStage &&
-                              (values.manipulations[index].manipulation.isNew ?
-                                availableProcedures : necessaryProcedures)[values.manipulations[index].manipulation.manipulationStage].map((manipulation, i) => {
-                                return <option key={i} value={manipulation}>{manipulation}</option>
-                              })
-                            }
-                          </Field>
-                          
-                          <Field className=" ml-2" placeholder="Количество"
-                                 name={`manipulations.${index}.manipulation.manipulationAmount`} type="number"
-                                 as={Input}/>
-                                 
-                          <Field
-                            className="ml-3 col-3"
-                            name={`manipulations.${index}.manipulation.manipulationUnits`}
-                            as={'select'}
-                          >
-                            <option value={''}>Eдиница измерения</option>
-        
-                            {
-                              units.map((unit, i) => {
+                        return (
+                          <div key={index}
+                               style={values.manipulations[index].manipulation.isNew ? {backgroundColor: 'rgba(38, 150, 38, 0.62)'} : null}
+                               className='d-flex p-2 mb-1'>
+                            
+                            <Field
+                              name={`manipulations.${index}.manipulation.manipulationStage`}
+                              as={'select'}
+                            >
+                              <option value={''}>Выберите этап</option>
+                              {
+                                Object.keys(values.manipulations[index].manipulation.isNew ? availableProcedures : necessaryProcedures).map((stage, i) => {
+                                  return <option key={i} value={stage}>{stage}</option>
+                                })}
+                            </Field>
+                            
+                            <Field
+                              className="ml-3"
+                              name={`manipulations.${index}.manipulation.manipulationName`}
+                              as={'select'}
+                            >
+                              <option value={''}>Выберите манипуляцию</option>
+                              {
+                                values.manipulations[index].manipulation.manipulationStage &&
+                                (values.manipulations[index].manipulation.isNew ?
+                                  availableProcedures : necessaryProcedures)[values.manipulations[index].manipulation.manipulationStage].map((manipulation, i) => {
+                                  return <option key={i} value={manipulation}>{manipulation}</option>
+                                })
+                              }
+                            </Field>
+                            
+                            <Field className=" ml-2" placeholder="Количество"
+                                   name={`manipulations.${index}.manipulation.manipulationAmount`} min={0} type="number"
+                                   as={Input}/>
+                            
+                            <Field
+                              className="ml-3 col-3"
+                              name={`manipulations.${index}.manipulation.manipulationUnits`}
+                              as={'select'}>
+                              <option value={''}>Eдиница измерения</option>
+                              
+                              {units.map((unit, i) => {
                                 return <option key={i} value={unit}>{unit}</option>
-                              })
-                            }
-                          </Field>
-                          
-                          <Button className="mb-2" close onClick={() => arrayHelpers.remove(index)}/>
-                        </div>
-                      );
-                    })}
+                              })}
+                            </Field>
+                            
+                            <Button className="mb-2" close onClick={() => arrayHelpers.remove(index)}/>
+                          </div>
+                        );
+                      })}
                     <div className='d-flex justify-content-between'>
-                      <Button color='primary' onClick={() => arrayHelpers.push({manipulation: {manipulationName: '', manipulationStage: '', manipulationAmount: 0, manipulationUnits: ''}})}>Добавить процедуру/упражнение</Button>
-                      <Button onClick={() => arrayHelpers.push({manipulation: {manipulationName: '', manipulationStage: '', manipulationAmount: 0, manipulationUnits: '', isNew: true}})}>Больше процедур</Button>
+                      <Button color='primary' onClick={() => arrayHelpers.push({
+                        manipulation: {
+                          manipulationName: '',
+                          manipulationStage: '',
+                          manipulationAmount: 0,
+                          manipulationUnits: ''
+                        }
+                      })}>Добавить процедуру/упражнение</Button>
+                      <Button onClick={() => arrayHelpers.push({
+                        manipulation: {
+                          manipulationName: '',
+                          manipulationStage: '',
+                          manipulationAmount: 0,
+                          manipulationUnits: '',
+                          isNew: true
+                        }
+                      })}>Больше процедур</Button>
                     </div>
-                    
+                  
                   </div>
                 )}
               </FieldArray>
@@ -118,13 +139,15 @@ class Attendance extends Component {
                         </div>
                       );
                     })}
-                    <Button className="d-block" color='primary' onClick={() => arrayHelpers.push({excerciseName: ""})}>Добавить упражнение</Button>
+                    <Button className="d-block" color='primary' onClick={() => arrayHelpers.push({excerciseName: ""})}>Добавить
+                      упражнение</Button>
                   </div>
                 )}
               </FieldArray>
-              <Field className="mb-3" placeholder="Состояние пациента после терапии" name="patienthood" type="input" as={Input}/>
+              <Field className="mb-3" placeholder="Состояние пациента после терапии" name="patienthood" type="input"
+                     as={Input}/>
               <p className="d-inline-block pr-3">Шкала боли</p>
-              <Field name="painScale"  as="select">
+              <Field name="painScale" as="select">
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
