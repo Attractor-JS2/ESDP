a73ed20202301d74dbcb0abcf4ec20a90b8ac347
yelkanat <elkanat01@gmail.com>
2020-04-03 19:36:28 +0600

    В файле index.js подключил store и history

--- rekinetix-client/src/index.js
+++ rekinetix-client/src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { store, history } from "./store/configureStore";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-datepicker/dist/react-datepicker.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-datepicker/dist/react-datepicker.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+const app = (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(app, document.getElementById("root"));
 serviceWorker.unregister();
