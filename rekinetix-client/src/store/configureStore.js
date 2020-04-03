d4646f781b3a4948f75b2001cfce4679661fd2a8
yelkanat <elkanat01@gmail.com>
2020-04-03 19:35:41 +0600

    В папке store создал файл configureStore.js, который  создает глобальный комбинированный стейт, подключил reactDevTools с помощью composeEnhancers

---
+++ rekinetix-client/src/store/configureStore.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { createBrowserHistory } from "history";
+import reducer from "./reducers/healingPlan";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import thunkMiddleware from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+  test: reducer,
+  router: connectRouter(history),
+});
+
+const middleware = [thunkMiddleware, routerMiddleware(history)];
+
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+export const store = createStore(rootReducer, persistedState, enhancers);
