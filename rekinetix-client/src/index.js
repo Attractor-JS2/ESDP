8818ef7ccd3a8eb91e757550dc7970a28286de69
yelkanat <elkanat01@gmail.com>
2020-04-04 16:36:17 +0600

    установил зависемости connected-react-router, history, connected, react-router

--- rekinetix-client/src/index.js
+++ rekinetix-client/src/index.js
@@ -4,16 +4,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { store, history } from "./store/configureStore";
+import { store } from "./store/configureStore";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 
 const app = (
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+    <App />
   </Provider>
 );
 // If you want your app to work offline and load faster, you can change
