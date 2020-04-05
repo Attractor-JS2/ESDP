bde5943d5032e61eb11b914d9fb8eb82909168ed
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-04-05 18:29:56 +0600

    Установил модуль react-google-charts

---
+++ rekinetix-client/src/components/ChartGantt.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Chart from 'react-google-charts';
+
+const ChartGantt = () => {
+  const [chartData, setChartData] = useState(null);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChartGantt;
