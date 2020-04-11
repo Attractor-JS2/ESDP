105499b748506b72e426c38471a2f7dc923d5973
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-04-11 23:12:00 +0600

    Удалил библиотеку react-google-charts и компоненты реализованные с ее помощью

--- rekinetix-client/src/App.js
+++ rekinetix-client/src/App.js
@@ -4,9 +4,7 @@ import {Route, Switch} from "react-router-dom";
 import Attendance from "./Containers/Attendance/Attendance";
 import HealingPlan from "./Containers/HealingPlan/HealingPlan";
 
-import ChartGantt from './components/ChartGantt/ChartGantt';
-import { healingPlanByPatient } from './components/ChartGantt/planData';
-import ChartTable from './components/ChartTable/ChartTable';
+import { healingPlanByPatient } from './components/HealingPlanChart/healingPlanData';
 import HealingPlanChart from './components/HealingPlanChart/HealingPlanChart';
 
 function App() {
@@ -14,12 +12,6 @@ function App() {
         <Switch>
           <Route path='/attendance' component={Attendance}/>
           <Route path='/plan' component={HealingPlan} />
-          <Route path='/chart'>
-            <ChartGantt healingPlan={healingPlanByPatient} />
-          </Route>
-          <Route path="/table">
-            <ChartTable healingPlan={healingPlanByPatient} />
-          </Route>
           <Route path="/react-table">
             <HealingPlanChart healingPlan={healingPlanByPatient} />
           </Route>
