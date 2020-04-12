dce0a322e2924c379a836d2dcdf205ee2a99560f
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-04-12 14:51:23 +0600

    Удалил роут для компонента графика плана лечения

--- rekinetix-client/src/App.js
+++ rekinetix-client/src/App.js
@@ -4,17 +4,12 @@ import {Route, Switch} from "react-router-dom";
 import Attendance from "./Containers/Attendance/Attendance";
 import HealingPlan from "./Containers/HealingPlan/HealingPlan";
 
-import { healingPlanByPatient } from './components/HealingPlanChart/healingPlanData';
-import HealingPlanChart from './components/HealingPlanChart/HealingPlanChart';
 
 function App() {
   return (
         <Switch>
           <Route path='/attendance' component={Attendance}/>
           <Route path='/plan' component={HealingPlan} />
-          <Route path="/react-table">
-            <HealingPlanChart healingPlan={healingPlanByPatient} />
-          </Route>
         </Switch>
   );
 }
