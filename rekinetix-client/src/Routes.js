53d60611ae96bf6d8f64ceefa1c2fe01a779cbe8
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-04-13 15:59:39 +0600

    Добавил роут для компонента графика плана лечения

--- rekinetix-client/src/Routes.js
+++ rekinetix-client/src/Routes.js
@@ -2,12 +2,17 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Attendance from "./Containers/Attendance/Attendance";
 import HealingPlan from "./Containers/HealingPlan/HealingPlan";
+import HealingPlanChart from "./components/HealingPlanChart/HealingPlanChart";
+import { healingPlanByPatient } from "./components/HealingPlanChart/healingPlanData";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/attendance" component={Attendance} />
       <Route path="/plan" component={HealingPlan} />
+      <Route path="/plan-chart">
+        <HealingPlanChart healingPlan={healingPlanByPatient} />
+      </Route>
     </Switch>
   );
 };
