64048d38b015bc07991cfbb6c511c781436af691
Northern Stormy <gerrardthebanana@gmail.com>
2020-04-24 01:18:37 +0600

    Добавил временный тулбар

--- rekinetix-client/src/App.js
+++ rekinetix-client/src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
 import Routes from "./Routes";
+import {Link} from "react-router-dom";
+import {Row} from "reactstrap";
 
 
 function App() {
-  return <Routes />;
+  return(
+  <>
+    <Row className='w-100 mb-3 border-bottom border-dark row justify-content-center'>
+      <Link className='mx-5' to='/attendance'>Прием</Link>
+      <Link to='/plan-chart'>План лечения</Link>
+    </Row>
+    
+    <Routes/>;
+  </>)
 }
 
 export default App;
