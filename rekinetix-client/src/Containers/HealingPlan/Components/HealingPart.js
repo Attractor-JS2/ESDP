fbc7537a3901b4bcae52808c2ae013252ddcf911
Northern Stormy <gerrardthebanana@gmail.com>
2020-04-24 01:21:56 +0600

    Заменил селекты на инпуты

--- rekinetix-client/src/Containers/HealingPlan/Components/HealingPart.js
+++ rekinetix-client/src/Containers/HealingPlan/Components/HealingPart.js
@@ -3,30 +3,27 @@ import {Field, FieldArray} from "formik";
 import {Button} from "reactstrap";
 
 const HealingPart = (props) => {
-
+  
   return (
     <>
       <FieldArray name={props.name}>
         {arrayHelpers => (
-          <div className="mb-3 mt-3"> <p className={props.userClassName}>{props.title}</p>
-            {props.partOne.map((firstPartData, index) => {
+          <div className="mb-3 mt-3"><p className={props.userClassName}>{props.title}</p>
+            {props.stage.map((firstPartData, index) => {
               return (
                 <div key={index} className="d-flex">
                   <Field
                     className="mb-2"
                     name={`${props.name}.${index}.${props.name}`}
-                    component="select"
-                  >
-                    {props.firstData}
-                  </Field>
+                    component="input"
+                  />
                   <label> Метод:
                     <Field
                       className="mb-2"
                       name={`${props.name}.${index}.${props.method}`}
-                      component="select"
-                    >
-                      {props.firstMethod}
-                    </Field>
+                      component="input"
+                    />
+                  
                   </label>
                   <Button className="mb-2" close onClick={() => arrayHelpers.remove(index)}/>
                 </div>
@@ -38,5 +35,5 @@ const HealingPart = (props) => {
       </FieldArray>
     </>
   )
-}
+};
 export default HealingPart;
