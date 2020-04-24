aa49552592d7706bfb97e37bba12fd5689aa39ec
Northern Stormy <gerrardthebanana@gmail.com>
2020-04-24 20:44:22 +0600

    #45 Привел компонент плана лечения к финальному варианту
    Закончил изменять названия переменных в соответствие с новым форматом, реализовал правильный проброс данных в дочерний компонент элемента формы

--- rekinetix-client/src/Containers/HealingPlan/HealingPlan.js
+++ rekinetix-client/src/Containers/HealingPlan/HealingPlan.js
@@ -6,18 +6,17 @@ import HealingPart from "./Components/HealingPart";
 
 class HealingPlan extends Component {
   render() {
-    const firstStageHealingItem=["Надостная мышца","Подостная мышца","Подлопаточная мышца","Малая круглая","Большая круглая","Широчайшая мышца спины","Малая грудная","Большая грудная","Подключичная","Поднимающая лопатку","Малая ромбовидная","Большая ромбовидная","Верхняя порция трапеции","Средняя порция трапеции","Нижняя порция трапеции","Передняя зубчатая","Задняя зубчатая","Дельтовидная передняя","Дельтовидная средняя","Дельтовидная задняя","Клювовидно плечевая","Длинная головка бицепса","Короткая головка бицепса","Трехглавая мышца плеча","Плечевая мышца (брохиалис)","Мышцы латерального мыщелка","Мышцы медиального мыщелка"];
-    const firstStageMethods=["Функциональный массаж"];
-    const secondStageHealingItem= ["Грудной отдел позвоночника","Шейный отдел позвоночника","Поясничный отдел позвоночника"];
-    const secondStageMethods=["Функциональный массаж","Нефункциональный массаж"];
-    const thirdStageHealingItem= ["Нижний цилиндр","Верхний цилиндр","Коленный сустав"];
-    const thirdStageMethods=["Функциональный массаж","Немассаж"];
-    const fourthStageHealingItem= ["Передняя лента","Задняя лента"];
-    const fourthStageMethods=["Функциональный массаж","Немассаж"];
-    const fifthStageHealingItem= ["-","Упражнения","Задания","Лекарства"];
-    const fifthStageMethods=["Ходьба","Бег", "Прыжки"];
-    const recommendationStageHealingItem=["Продолжать заниматься"];
-    const recommendationStageMethods=["Не сдаваться"];
+    const firstStageProcedureAreas=["Надостная мышца","Подостная мышца","Подлопаточная мышца","Малая круглая","Большая круглая","Широчайшая мышца спины","Малая грудная","Большая грудная","Подключичная","Поднимающая лопатку","Малая ромбовидная","Большая ромбовидная","Верхняя порция трапеции","Средняя порция трапеции","Нижняя порция трапеции","Передняя зубчатая","Задняя зубчатая","Дельтовидная передняя","Дельтовидная средняя","Дельтовидная задняя","Клювовидно плечевая","Длинная головка бицепса","Короткая головка бицепса","Трехглавая мышца плеча","Плечевая мышца (брохиалис)","Мышцы латерального мыщелка","Мышцы медиального мыщелка"];
+    const firstStageProcedureNames=["Функциональный массаж"];
+    const secondStageProcedureAreas= ["Грудной отдел позвоночника","Шейный отдел позвоночника","Поясничный отдел позвоночника"];
+    const secondStageProcedureNames=["Функциональный массаж","Нефункциональный массаж"];
+    const thirdStageProcedureAreas= ["Нижний цилиндр","Верхний цилиндр","Коленный сустав"];
+    const thirdStageProcedureNames=["Функциональный массаж","Немассаж"];
+    const fourthStageProcedureAreas= ["Передняя лента","Задняя лента"];
+    const fourthStageProcedureNames=["Функциональный массаж","Немассаж"];
+    const fifthStageProcedureAreas= ["Упражнения","Задания","Лекарства"];
+    const fifthStageProcedureNames=["Ходьба","Бег", "Прыжки"];
+   
     
 
     return (
@@ -38,37 +37,41 @@ class HealingPlan extends Component {
             <Form>
               <HealingPart
                 name = "firstStageData"
-                // methods = {firstStageMethods}
-                // names = {firstStageHealingItem}
-                userClassName = "firstPart"
+                procedureNames={firstStageProcedureNames}
+                procedureAreas={firstStageProcedureAreas}
+                stageClassName ={{title: "firstStageTitle", background: 'firstStageBg'}}
                 title = "Этап 1: Обезболивание/противовоспалительные мероприятия"
                 stage = {values.firstStageData}
               />
               <HealingPart
                 name = "secondStageData"
-               
-                userClassName = "secondPart"
+                procedureNames={secondStageProcedureNames}
+                procedureAreas={secondStageProcedureAreas}
+                stageClassName = {{title: "secondStageTitle", background: 'secondStageBg'}}
                 title = "Этап 2: Мобилизация"
                 stage = {values.secondStageData}
               />
               <HealingPart
                 name = "thirdStageData"
-               
-                userClassName = "thirdPart"
+                procedureNames={thirdStageProcedureNames}
+                procedureAreas={thirdStageProcedureAreas}
+                stageClassName = {{title: "thirdStageTitle", background: 'thirdStageBg'}}
                 title = "Этап 3: НМА и Стабилизация"
                 stage = {values.thirdStageData}
               />
               <HealingPart
                 name = "fourthStageData"
-               
-                userClassName = "fourthPart"
+                procedureNames={fourthStageProcedureNames}
+                procedureAreas={fourthStageProcedureAreas}
+                stageClassName = {{title: "fourthStageTitle", background: 'fourthStageBg'}}
                 title = "Этап 4: Восстановление функций в МФЛ"
                 stage = {values.fourthStageData}
               />
               <HealingPart
                 name = "fifthStageData"
-              
-                userClassName = "fifthPart"
+                procedureNames={fifthStageProcedureNames}
+                procedureAreas={fifthStageProcedureAreas}
+                stageClassName = {{title: "fifthStageTitle", background: 'fifthStageBg'}}
                 title = "Этап 5: Профилактика"
                 stage = {values.fifthStageData}
               />
