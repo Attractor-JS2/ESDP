4e34a2603ff848e3f752dffe4734e2584fcf385b
Northern Stormy <gerrardthebanana@gmail.com>
2020-04-24 19:27:37 +0600

    Добавил возможность вписывать планируемое количество выполений процедуры
    Также изменил внешнее оформление

--- rekinetix-client/src/Containers/HealingPlan/Components/HealingPart.js
+++ rekinetix-client/src/Containers/HealingPlan/Components/HealingPart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Field, FieldArray} from "formik";
 import {Button} from "reactstrap";
-import {Input} from "@material-ui/core";
+import {TextField} from "@material-ui/core";
 
 const HealingPart = (props) => {
   
@@ -9,22 +9,40 @@ const HealingPart = (props) => {
     <>
       <FieldArray name={props.name}>
         {arrayHelpers => (
-          <div className="mb-3 mt-3"><p className={props.userClassName}>{props.title}</p>
+          <div className={`mb-3 mt-4 pb-3 border-bottom border-dark ${props.stageClassName.background}`}><p className={props.stageClassName.title}>{props.title}</p>
             {props.stage.map((procedure, index) => {
               return (
-                <div key={index} className="d-flex">
-                  <Field
-                    className="mb-2"
-                    name={`${props.name}[${index}].procedureName`}
-                    type='input'
-                    as={Input}
-                  />
-                  <label> Метод:
+                <div key={index} className="d-flex justify-content-between border-bottom mb-3">
+                  <label className='d-flex flex-column w-25'> Название процедуры:
+                    
+                    
                     <Field
                       className="mb-2"
+                      name={`${props.name}[${index}].procedureName`}
+                      placeholder='Введите название процедуры'
+                      type='input'
+                      as={TextField}
+                    />
+                  </label>
+                  <label className='d-flex flex-column w-25'> Область направления процедуры:
+                    <Field
+                      className="mb-2 flex-wrap"
+                      placeholder='Введите область направления'
+                      
                       name={`${props.name}[${index}].procedureArea`}
                       type='input'
-                      as={Input}
+                      as={TextField}
+                    />
+                  
+                  </label>
+                  <label className='d-flex flex-column'> Планируемое кол-во выполнений:
+                    <Field
+                      className="mb-2"
+                      name={`${props.name}[${index}].procedureQuantity`}
+                      type='number'
+                      inputProps={{ min: "0", step: "1" }}
+                      min='12'
+                      as={TextField}
                     />
                   
                   </label>
@@ -32,7 +50,7 @@ const HealingPart = (props) => {
                 </div>
               );
             })}
-            <Button className="d-block" onClick={() => arrayHelpers.push({})}>Добавить</Button>
+            <Button color='primary' className="d-block" onClick={() => arrayHelpers.push({})}>Добавить</Button>
           </div>
         )}
       </FieldArray>
