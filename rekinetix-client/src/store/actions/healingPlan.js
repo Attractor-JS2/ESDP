6061695f0ec390b9b5e58fa3fce0e1e8623f2580
Northern Stormy <gerrardthebanana@gmail.com>
2020-04-25 17:46:08 +0600

    #47 Реализовал библиотеку react-notifications
    Установил библиотеку, подулючил css в index.js, контейнер для отображения оповещений возле тулбара в app.js и триггер для срабатывания оповещения на успешном post - запросе на бэк

--- rekinetix-client/src/store/actions/healingPlan.js
+++ rekinetix-client/src/store/actions/healingPlan.js
@@ -1,10 +1,12 @@
 import axios from '../../axiosBackendInstance';
+import {NotificationManager} from "react-notifications";
 
 export const submitForm = (form) => {
   return () => {
     axios.post('/healingPlan', {healingPlan: form})
       .then(res => {
-        console.log(res)
+        console.log(res);
+        NotificationManager.success('Форма успешно отправлена');
       });
   };
 };
