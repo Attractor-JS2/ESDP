18e9bb596d44bd28dd69fa7c32e3be8f1f0dbd1c
Northern Stormy <gerrardthebanana@gmail.com>
2020-04-25 16:10:01 +0600

    #47 Создал обработчики запросов на план лечения
    Добавил обработчик get-запроса, который возвращает все записи в JSON-файле
    Добавил обработчик post-запроса, который делает запись в JSON-файле, основываясь на ключе healingPlan в запросе

--- rekinetix-backend/controllers/healingPlan/healingPlan.js
+++ rekinetix-backend/controllers/healingPlan/healingPlan.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
+const db = require('./db/db');
+
+db.init();
 
 const createRouter = () => {
   router.get('/', (req, res) => {
-    res.send('hello there')
+    const data = db.getData();
+    res.send(data)
   });
   
   router.post('/', (req, res) => {
-  
+    const healingPlan = req.body.healingPlan;
+    try {
+      db.addItem(healingPlan);
+    } catch (e) {
+      return res.sendStatus(500);
+    }
+    res.status(200).send(`successfully added`)
   });
   
   return router;
