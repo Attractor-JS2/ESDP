52c61f5970ab43b7881cf8206f86266551eb10e8
Northern Stormy <gerrardthebanana@gmail.com>
2020-04-25 15:37:44 +0600

    #47 Создал базовую структуру эндпоинта для плана лечения
    Также подключил его в server.js

--- rekinetix-backend/server.js
+++ rekinetix-backend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const healingPlan = require('./controllers/healingPlan/healingPlan');
 const PORT = 8000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/healingPlan', healingPlan());
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT} port`);
