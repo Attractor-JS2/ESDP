698af2b5360251c624c1133d415535374bf5e831
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-04-27 13:27:28 +0600

    Добавил контейнер

--- rekinetix-client/src/components/Header/Header.js
+++ rekinetix-client/src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { NavLink as RouterNavLink } from "react-router-dom";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import { NavLink as RouterNavLink } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
+
 import "./Header.css";
 import Logo from "../Logo/Logo";
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
       flexGrow: 1,
@@ -31,21 +34,25 @@ export default function Header() {
   return (
     <div className={classes.root}>
       <AppBar position="static">
-        <Toolbar>
-          <Logo />
-          <Typography variant="h6" className={classes.title}>
-            <RouterNavLink className={classes.menuLink} to="/attendance" exact>
-              Отчет по приёму
-            </RouterNavLink>
-            <RouterNavLink className={classes.menuLink} to="/plan" exact>
-              План лечения{" "}
-            </RouterNavLink>
-            <RouterNavLink className={classes.menuLink} to="/plan-chart" exact>
-              Таблица плана лечения
-            </RouterNavLink>
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+        <ScopedCssBaseline>
+          <Container>
+            <Toolbar>
+              <Logo />
+              <Typography variant="h6" className={classes.title}>
+                <RouterNavLink className={classes.menuLink} to="/attendance" exact>
+                  Отчет по приёму
+                </RouterNavLink>
+                <RouterNavLink className={classes.menuLink} to="/plan" exact>
+                  План лечения{" "}
+                </RouterNavLink>
+                <RouterNavLink className={classes.menuLink} to="/plan-chart" exact>
+                  Таблица плана лечения
+                </RouterNavLink>
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </Container>
+        </ScopedCssBaseline>
       </AppBar>
     </div>
   );
