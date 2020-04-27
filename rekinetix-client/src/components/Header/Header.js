0eba742efd87af274a019331c49c1891a3350851
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-04-27 14:06:05 +0600

    Обновил зависимости и удалил лишние импорты

--- rekinetix-client/src/components/Header/Header.js
+++ rekinetix-client/src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink as RouterNavLink } from "react-router-dom";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
