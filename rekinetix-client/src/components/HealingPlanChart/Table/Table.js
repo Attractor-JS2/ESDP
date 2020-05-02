32e35759142babf6e2dcbaf832a9bd6bc7c5ef4e
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-05-02 16:03:13 +0600

    Добавил возможность получения данных выбраных строк

--- rekinetix-client/src/components/HealingPlanChart/Table/Table.js
+++ rekinetix-client/src/components/HealingPlanChart/Table/Table.js
@@ -6,23 +6,32 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CheckboxCell from './Checkbox/Checkbox';
+import { TableContainer } from '@material-ui/core';
 
-const Table = ({ columns, data, addProcedureHandler, proceedToDeleteProcedure, updateSelectData }) => {
+const Table = ({
+  columns,
+  data,
+  addProcedureHandler,
+  proceedToDeleteProcedure,
+  updateSelectData,
+}) => {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
-  } = useTable({
-    columns,
-    data,
-    addProcedureHandler,
-    proceedToDeleteProcedure,
-    updateSelectData,
-  },
+    state: { selectedRowIds },
+  } = useTable(
+    {
+      columns,
+      data,
+      addProcedureHandler,
+      proceedToDeleteProcedure,
+      updateSelectData,
+    },
     useRowSelect,
-    hooks => {
+    (hooks) => {
       hooks.allColumns.push((columns) => [
         {
           id: 'selection',
@@ -38,34 +47,55 @@ const Table = ({ columns, data, addProcedureHandler, proceedToDeleteProcedure, u
           ),
         },
         ...columns,
-      ])
-    }
+      ]);
+    },
   );
 
+  const getDataByIndexes = (array, indexes) =>
+    array.filter((_, id) => indexes.includes(id));
+
+  const getSelectedRows = () => {
+    console.log(selectedRowIds);
+    const selectedRows = getDataByIndexes(
+      data,
+      Object.keys(selectedRowIds).map((x) => parseInt(x, 10)),
+    );
+    console.dir(selectedRows);
+  };
+
   return (
-    <MaUTable {...getTableProps()} size="small" >
-      <TableHead>
-        {headerGroups.map((headerGroup) => (
-          <TableRow {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </TableRow>
-        ))}
-      </TableHead>
-      <TableBody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <TableRow {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <TableCell align="left" {...cell.getCellProps()}>{cell.render("Cell")}</TableCell>;
-              })}
+    <TableContainer>
+      <div>
+        <button onClick={getSelectedRows}>Log</button>
+      </div>
+      <MaUTable {...getTableProps()} size="small">
+        <TableHead>
+          {headerGroups.map((headerGroup) => (
+            <TableRow {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
             </TableRow>
-          );
-        })}
-      </TableBody>
-    </MaUTable>
+          ))}
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <TableRow {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <TableCell align="left" {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </MaUTable>
+    </TableContainer>
   );
 };
 
