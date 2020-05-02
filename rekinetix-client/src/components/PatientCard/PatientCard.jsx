625087192d596f6feadb6cea61b653ae87d80cc6
yelkanat <elkanat01@gmail.com>
2020-05-02 17:41:42 +0600

    создал компонент PatientCard
    импортировал   все что нудно катрочке из material-ui

---
+++ rekinetix-client/src/components/PatientCard/PatientCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import CardMedia from "@material-ui/core/CardMedia";
+import defaultImage from "../../assets/defaultImage/default-photo.jpeg";
