3d43c75a57ef6b2e822a8fb6e2dabb1fa8258b7e
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-04 00:22:28 +0600

    #69 Создал редюсер для карт пациентов
    Создал редюсер с элементарной структурой, подключил в combinedReducer

--- rekinetix-client/src/store/configureStore.js
+++ rekinetix-client/src/store/configureStore.js
@@ -2,6 +2,7 @@ import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { createBrowserHistory } from "history";
 import healingPlanReducer from "./reducers/healingPlan";
 import attendance from "./reducers/attendance";
+import patientCardsReducer from "./reducers/patientCards";
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import thunkMiddleware from "redux-thunk";
 
@@ -12,6 +13,7 @@ export const history = createBrowserHistory();
 const rootReducer = combineReducers({
   healingPlan: healingPlanReducer,
   attendance: attendance,
+  patientCards: patientCardsReducer,
   router: connectRouter(history),
 });
 
---
+++ rekinetix-client/src/store/reducers/patientCards.js
@@ -0,0 +1,14 @@
+const initialState = {
+  patientCards: {},
+  currentCard: {}
+};
+
+const patientCards = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  
+  }
+};
+
+export default patientCards;
