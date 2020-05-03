96aaf2c3c1a9d7b571bd6a028a0427d032fcab47
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-04 00:13:40 +0600

    #68 Подключил обработку контроллера на роут

--- rekinetix-backend/server.js
+++ rekinetix-backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const healingPlan = require('./controllers/healingPlan/healingPlan');
 const attendance = require('./controllers/attendance/attendance');
+const patientCard = require('./controllers/patientCard/patientCard');
 const PORT = 8000;
 
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(cors());
 
 app.use('/attendance', attendance());
 app.use('/healingPlan', healingPlan());
+app.use('/patientCards', patientCard());
 
 app.listen(PORT, () => {
   console.log(`Server started at ${PORT} port`);
