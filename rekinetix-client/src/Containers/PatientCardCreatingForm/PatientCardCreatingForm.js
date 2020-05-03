6a77683341a74bcb35c778ec9c21f2a405d43b37
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-03 23:44:36 +0600

    #67 Закончил работу над формой создания карты пациента

--- rekinetix-client/src/Containers/PatientCardCreatingForm/PatientCardCreatingForm.js
+++ rekinetix-client/src/Containers/PatientCardCreatingForm/PatientCardCreatingForm.js
@@ -1,44 +1,68 @@
 import React, {Component} from "react";
 import {Button, Container} from "reactstrap";
-import {Formik, Form} from "formik";
+import {Formik, Form, Field} from "formik";
 import {submitForm} from "../../store/actions/healingPlan";
 import {connect} from "react-redux";
 import ModifiedInput from "./Components/ModifiedInput";
+import TextField from "@material-ui/core/TextField";
+import DatePicker from 'react-date-picker';
 
-class HealingPlan extends Component {
+class PatientCardCreatingForm extends Component {
   render() {
-   
     const redFlags = ["Упражнения", "Задания", "Лекарства"];
     const diagnosis = ["Ходьба", "Бег", "Прыжки"];
     
-    
     return (
       <Container className="mt-5">
-        <h3>План лечения</h3>
+        <h3>Новый пациент</h3>
         <Formik
           initialValues={{
-            firstStage: [],
-            secondStage: [],
-            thirdStage: [],
+            patientName: '',
+            birthDate: new Date(),
             redFlags: [],
             diagnosis: []
           }}
           onSubmit={data => {
-            this.props.submitForm(data);
+            console.log(data)
           }}>
-          {({values}) => (
+          {({values, handleChange}) => (
             <Form>
-             
+              <div className='mb-3'>
+                <Field
+                  name="patientName"
+                  as={TextField}
+                  label='ФИО пациента'
+                />
+              </div>
+              <div>
+                <DatePicker
+                  // as={DatePicker}
+                  
+                  onChange={(val) => {
+                    val = val.toString();
+                    console.log(val);
+                    return handleChange(val);
+                  }}
+                  name="birthDate"
+                  disableCalendar
+                />
+              </div>
               <ModifiedInput
                 name="redFlags"
                 autocompleteOptions={redFlags}
                 label="Красные флаги"
                 values={values.redFlags}
               />
+              <ModifiedInput
+                name="diagnosis"
+                autocompleteOptions={diagnosis}
+                label="Диагноз"
+                values={values.diagnosis}
+              />
               
               <Button type="submit">Сохранить</Button>
               <pre>{JSON.stringify(values, null, 2)}</pre>
-
+            
             </Form>
           )}
         </Formik>
@@ -53,4 +77,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(HealingPlan);
+export default connect(null, mapDispatchToProps)(PatientCardCreatingForm);
