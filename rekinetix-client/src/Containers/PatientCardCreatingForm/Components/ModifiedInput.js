c14bba265be542093096ce5f207f2533431d2a2b
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-03 22:40:07 +0600

    #67 Изменил компонент добавления и удаления полей формы под нужный формат
    Удалил лишние поля, изменил названия пропсов, переделал структуру массива объектов на массив строк

--- rekinetix-client/src/Containers/PatientCardCreatingForm/Components/ModifiedInput.js
+++ rekinetix-client/src/Containers/PatientCardCreatingForm/Components/ModifiedInput.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import {Field, FieldArray} from "formik";
 import {Button} from "reactstrap";
-import {TextField} from "@material-ui/core";
 import FormikAutocomplete from "./FormikAutocomplete";
 
-const HealingPart = (props) => {
+const ModifiedInput = (props) => {
   
-  const procedureNameAutocompleteProps = {
-    options: props.procedureNames,
-    getOptionLabel: option => option,
-  };
-  
-  const procedureAreaAutocompleteProps = {
-    options: props.procedureAreas,
+  const arrayToAutocomplete = {
+    options: props.autocompleteOptions,
     getOptionLabel: option => option,
   };
   
@@ -20,55 +14,33 @@ const HealingPart = (props) => {
     <>
       <FieldArray name={props.name}>
         {arrayHelpers => (
-          <div className={`mb-3 mt-4 pb-3 border-bottom border-dark ${props.stageClassName.background}`}><p className={props.stageClassName.title}>{props.title}</p>
-            {props.stage.map((procedure, index) => {
+          <div className={`mb-3 mt-4 pb-3 border-bottom border-dark`}><p>{props.title}</p>
+            {props.values.map((procedure, index) => {
               return (
                 <div key={index} className="d-flex justify-content-between border-bottom mb-3">
                   <label className='d-flex flex-column w-25'> Название процедуры:
                     
                     <Field
-                      name={`${props.name}[${index}].procedureName`}
+                      name={`${props.name}[${index}]`}
                       component={FormikAutocomplete}
-                      label="Введите название процедуры"
+                      label={props.label}
                       clearOnEscape
-                      {...procedureNameAutocompleteProps}
+                      {...arrayToAutocomplete}
                     
                     />
                   
                   
                   </label>
-                  <label className='d-flex flex-column w-25'> Область направления процедуры:
-                    
-                    <Field
-                      name={`${props.name}[${index}].procedureArea`}
-                      component={FormikAutocomplete}
-                      label="Введите область направления"
-                      clearOnEscape
-                      {...procedureAreaAutocompleteProps}
-                    
-                    />
                   
-                  </label>
-                  <label className='d-flex flex-column'> Планируемое кол-во выполнений:
-                    <Field
-                      className="mb-2"
-                      name={`${props.name}[${index}].procedureQuantity`}
-                      type='number'
-                      inputProps={{ min: "0", step: "1" }}
-                      min='12'
-                      as={TextField}
-                    />
-                  
-                  </label>
                   <Button className="mb-2" close onClick={() => arrayHelpers.remove(index)}/>
                 </div>
               );
             })}
-            <Button color='primary' className="d-block" onClick={() => arrayHelpers.push({procedureName: '', procedureArea: '', procedureQuantity: 0})}>Добавить</Button>
+            <Button color='primary' className="d-block" onClick={() => arrayHelpers.push('')}>Добавить</Button>
           </div>
         )}
       </FieldArray>
     </>
   )
 };
-export default HealingPart;
+export default ModifiedInput;
