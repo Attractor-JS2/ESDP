17ccba26faab150c09b0af3266e2f06e26663496
yelkanat <elkanat01@gmail.com>
2020-05-11 14:35:34 +0600

    создал модель для поциента добавил все необходимые поля
    Добавил опцию timestamp для схемы.

---
+++ rekinetix-backend/models/Patient.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PatientSchema = new Schema(
+  {
+    fullname: {
+      type: String,
+      required: true,
+    },
+    birthday: {
+      type: Date,
+      required: true,
+    },
+    floor: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Patient = mongoose.model("Patient", PatientSchema);
+
+module.exports = Patient;
