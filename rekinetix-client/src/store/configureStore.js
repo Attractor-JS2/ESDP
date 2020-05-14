0381f8406f9f09a195fd5a8bc15321943f9fd9fd
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-05-14 17:28:12 +0600

    Добавил редьюсер для данных пользователя #90

--- rekinetix-client/src/store/configureStore.js
+++ rekinetix-client/src/store/configureStore.js
@@ -1,10 +1,13 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import { createBrowserHistory } from "history";
+
+import { loadStateFromLocalStorage, saveStateToLocalStorage } from "./localStorage";
 import healingPlanReducer from "./reducers/healingPlan";
 import attendance from "./reducers/attendance";
 import patientCardsReducer from "./reducers/patientCards";
-import { connectRouter, routerMiddleware } from "connected-react-router";
-import thunkMiddleware from "redux-thunk";
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -21,4 +24,14 @@ const middleware = [thunkMiddleware, routerMiddleware(history)];
 
 const enhancers = composeEnhancers(applyMiddleware(...middleware));
 
+const persistedState = loadStateFromLocalStorage();
+
 export const store = createStore(rootReducer, enhancers);
+
+store.subscribe(() => {
+  saveStateToLocalStorage({
+    users: {
+      user: store.getState().users.user,
+    },
+  });
+});
