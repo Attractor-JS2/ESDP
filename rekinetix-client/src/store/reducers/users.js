3ca0992720c50ff810bc83f5424be180bd2b459b
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-05-14 17:48:36 +0600

    Добавил экшены для регистрации и логина

--- rekinetix-client/src/store/reducers/users.js
+++ rekinetix-client/src/store/reducers/users.js
@@ -1,9 +1,29 @@
+import {
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAILURE,
+  LOGIN_USER_FAILURE,
+  LOGIN_USER_SUCCESS,
+  LOGOUT_USER,
+} from '../actions/actionTypes';
+
 const initialState = {
   user: null,
+  registerError: null,
+  loginError: null,
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case REGISTER_USER_SUCCESS:
+      return { ...state, registerError: null };
+    case REGISTER_USER_FAILURE:
+      return { ...state, registerError: payload };
+    case LOGIN_USER_SUCCESS:
+      return { ...state, loginError: null, user: { ...payload } };
+    case LOGIN_USER_FAILURE:
+      return { ...state, loginError: payload };
+    case LOGOUT_USER:
+      return { ...state, user: null };
     default:
       return state;
   }
