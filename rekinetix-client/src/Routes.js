07e47be0c0f178338def2da38697db9ba8ea2cd3
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-05-18 16:11:18 +0600

    Добавил проверку роли пользователя на роут формы регистрации

--- rekinetix-client/src/Routes.js
+++ rekinetix-client/src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Attendance from "./Containers/Attendance/Attendance";
 import HealingPlan from "./Containers/HealingPlan/HealingPlan";
 import HealingPlanChart from "./components/HealingPlanChart/HealingPlanChart";
@@ -15,8 +15,13 @@ import PrimaryAssessment from "./Containers/PrimaryAssessment/PrimaryAssessment"
 import Login from "./Containers/Login/Login";
 import Register from "./Containers/Register/Register";
 import SiteMap from "./Containers/SiteMap/SiteMap";
+import { connect } from "react-redux";
 
-const Routes = () => {
+const ProtectedRoute = ({ isAllowed, ...props }) => (
+  isAllowed ? <Route {...props} /> : <Redirect to="/" />
+);
+
+const Routes = ({ user }) => {
   return (
     <Switch>
       <Route path="/primary-assessment" component={PrimaryAssessment} />
@@ -34,11 +39,19 @@ const Routes = () => {
       </Route>
 
       <Route path="/" exact component={Login} />
-      <Route path="/register" component={Register} />
+      <ProtectedRoute
+        path="/register"
+        component={Register}
+        isAllowed={user && user.role === "admin"}
+      />
 
       <Route path="/siteMap" component={SiteMap} />
     </Switch>
   );
 };
 
-export default Routes;
+const mapStateToProps = (state) => ({
+  user: state.users.user,
+});
+
+export default connect(mapStateToProps)(Routes);
