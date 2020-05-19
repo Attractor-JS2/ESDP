e52121e9a27f00bafa0ee4d20252710e188ae0a7
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-20 01:50:16 +0600

    #78 Исправил опечатку

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 cd rekinetix-client || exit 13
-docker run --rm -it -v -e BACKEND_URL=https://api.rekinetix.ajs2.esdp.almaty.attractor.school $(pwd -P):/app node:10.20.1 bash -c "cd app && yarn && yarn build"
+docker run --rm -it -e BACKEND_URL=https://api.rekinetix.ajs2.esdp.almaty.attractor.school -v $(pwd -P):/app node:10.20.1 bash -c "cd app && yarn && yarn build"
 rm -rf /var/www/frontend
 mv build/ /var/www/frontend
 cd - || exit 13
