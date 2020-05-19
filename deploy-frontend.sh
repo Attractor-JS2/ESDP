102f0079bc6e8c2d373b6d2042f1bfd141e137b2
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-20 01:51:51 +0600

    #78 Включил строгий режим исполнения скрипта

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+set -e
 cd rekinetix-client || exit 13
 docker run --rm -it -e BACKEND_URL=https://api.rekinetix.ajs2.esdp.almaty.attractor.school -v $(pwd -P):/app node:10.20.1 bash -c "cd app && yarn && yarn build"
 rm -rf /var/www/frontend
