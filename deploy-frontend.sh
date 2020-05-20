da0c57d656e17cf20fef2ac97c5348eb42e3c6dd
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-20 19:45:00 +0600

    #78 Использовал новый вариант передачи переменной окружения при сборке

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
-BACKEND_URL=https://api.rekinetix.ajs2.esdp.almaty.attractor.school
+
 set -e
 cd rekinetix-client || exit 13
-sed -i "s/baseURL:.*$/baseURL: '${BACKEND_URL}'" src/axiosBackendInstance.js
-docker run --rm -it  -v $(pwd -P):/app node:10.20.1 bash -c "cd app && yarn && yarn build"
+docker run --rm -it -v $(pwd -P):/app node:10.20.1 bash -c "cd app && yarn && yarn build"
 rm -rf /var/www/frontend
 mv build/ /var/www/frontend
 git checkout src/axiosBackendInstance.js
