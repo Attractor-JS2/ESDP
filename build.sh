f2650717acf8589fb1c3bb62c84d45b2efd725aa
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-21 01:17:28 +0600

    #78 Добавил параметр -o StrictHostKeyChecking=no

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+set -ex
 ssh_key=~/.ssh/id_rsa
 [[ -f /opt/bitnami/jenkins/jenkins_home/.ssh/id_rsa ]] && ssh_key=/opt/bitnami/jenkins/jenkins_home/.ssh/id_rsa
 
-ssh -v -tt -i ${ssh_key} root@rekinetix.ajs2.esdp.almaty.attractor.school <<SSH
+ssh -o StrictHostKeyChecking=no -tt -i ${ssh_key} root@rekinetix.ajs2.esdp.almaty.attractor.school <<SSH
   set -ex
   cd ESDP
   ls -al
