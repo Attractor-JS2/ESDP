548327f0eaa6fcb87b48e7812b377dd0462c395f
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-20 14:26:25 +0600

    #78 Внедрил пробное решение по замене адреса бэкенда

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
+BACKEND_URL=https://api.rekinetix.ajs2.esdp.almaty.attractor.school
 set -e
 cd rekinetix-client || exit 13
-docker run --rm -it -e BACKEND_URL=https://api.rekinetix.ajs2.esdp.almaty.attractor.school -v $(pwd -P):/app node:10.20.1 bash -c "cd app && yarn && yarn build"
+sed -i "s/baseURL:.*$/baseURL: '${BACKEND_URL}'" src/axiosBackendInstance.js
+docker run --rm -it  -v $(pwd -P):/app node:10.20.1 bash -c "cd app && yarn && yarn build"
 rm -rf /var/www/frontend
 mv build/ /var/www/frontend
+git checkout src/axiosBackendInstance.js
 cd - || exit 13
