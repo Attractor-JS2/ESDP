5f34748b148e2dac6972ff7e49f5f060095b60c1
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-21 00:00:12 +0600

    #78 Добавил скрипт для запуска сборки из jenkins

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+ssh -tt root@rekinetix.ajs2.esdp.almaty.attractor.school <<SSH
+  set -ex
+  cd ESDP
+  ls -al
+  git reset --hard
+  git checkout feature-deployment
+  git status
+  git pull
+  ./deploy-backend.sh
+  ./deploy-frontend.sh
+SSH
