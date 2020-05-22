19be4ff890a3b1501b0ed0b4bed394c162ea80ba
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-05-22 21:42:40 +0600

    Добавил фикстуры по одному на документу на коллекцию

--- rekinetix-backend/fixtures.js
+++ rekinetix-backend/fixtures.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 const User = require('./models/User');
+const Patient = require('./models/Patient');
+const Procedure = require('./models/Procedure');
+const ObjectiveExam = require('./models/PrimaryAssessment/ObjectiveExam');
+const PrimaryAssessment = require('./models/PrimaryAssessment/PrimaryAssessment');
+const HealingPlan = require('./models/HealingPlan');
+const Attendance = require('./models/Attendance');
 
 mongoose.connect(config.db.getDbPath(), {
   useNewUrlParser: true,
@@ -13,17 +19,133 @@ const db = mongoose.connection;
 db.once('open', async () => {
   try {
     await db.dropCollection('users');
+    await db.dropCollection('patients');
+    await db.dropCollection('procedures');
+    await db.dropCollection('objectiveexams');
+    await db.dropCollection('primaryassessments');
+    await db.dropCollection('healingplans');
+    await db.dropCollection('attendances');
   } catch (error) {
     console.log(error);
   }
 
-  const testUser = await User.create({
-    fullname: 'testuser',
-    username: 'testuser',
-    password: 'testuser',
-    role: 'admin',
-  });
-  console.log(testUser);
+  try {
+    const testUser = await User.create({
+      fullname: 'testuser',
+      username: 'testuser',
+      password: 'testuser',
+      role: 'admin',
+    });
+    console.log(testUser);
+
+    const testPatient = await Patient.create({
+      fullname: 'Сидоров Иван Петрович',
+      birthday: new Date(1970, 1, 2).toISOString(),
+      gender: 'мужской',
+      height: '178',
+      weight: '78',
+      phone: '77057778899',
+      address: 'Казыбек би 47, 51',
+    });
+    console.log(testPatient);
+
+    const objectiveExam = await ObjectiveExam.create({
+      foot: {
+        D: 'вальгус',
+        S: 'вальгус',
+        additionalInfo: '',
+      },
+      hip: {
+        D: '',
+        S: '',
+        additionalInfo: '',
+      },
+      pelvicSpine: {
+        D: 'передний наклон',
+        S: 'передний наклон',
+        additionalInfo: '',
+      },
+      lumbarSpine: {
+        D: 'гиперлордоз',
+        S: 'гиперлордоз',
+        additionalInfo: '',
+      },
+      thoracicSpine: {
+        D: 'гиперкифоз',
+        S: 'гиперкифоз',
+        additionalInfo: '',
+      },
+      shoulderGirdel: {
+        D: 'Отведение. Внутрення ротация плеча',
+        S: 'Отведение',
+        additionalInfo: 'Провокационные тесты на плечевой сустав отрицательные',
+      },
+      headAndNeck: {
+        D: 'протракция',
+        S: 'протракция',
+        additionalInfo: '',
+      },
+    });
+    console.log(objectiveExam);
+
+    const primaryAssessment = await PrimaryAssessment.create({
+      patient: testPatient.id,
+      attendingDoctor: testUser.id,
+      datetime: new Date().toISOString(),
+      complaints: 'Боли в пояснице',
+      anamnesisVitae: 'Сидячий образ жизни',
+      anamnesisMorbi:
+        'Беспокоит в течении многих лет. Лечения и диагностики не было.',
+      objectiveExam: objectiveExam.id,
+      examinations: '',
+      diagnosis:
+        '«Внутренняя ротация плеча. Тендиноз собственной связки надколенника. Верхний и нижний кросс синдром.» 1) Мягкие мануальные техники ( глубокий мануальный массаж, терапия триггерных точек, растяжение мышц ) — внутренние ротаторы плеча, верхняя трапеция, четырехлавая мышца бедра. 2) Кинезитерапия — коррекция осанки, стабилизация лопаток, плечевого и коленного сустава. 3) Коррекция стельками Родион Фомин 8 747 587 43 67',
+    });
+    console.log(primaryAssessment);
+
+    const healingPlan = await HealingPlan.create({
+      primaryAssessment: primaryAssessment.id,
+    });
+    console.log(healingPlan);
+
+    const procedure = await Procedure.create({
+      stage: 1,
+      procedureArea: 'Плечо: левое',
+      procedureName: 'ММТ: ПИР',
+      comments: '60 дж 8гц 1500 уд',
+      status: 'запланировано',
+      procedureQuantity: 4,
+      healingPlan: healingPlan.id,
+      patient: testPatient._id,
+      medic: testUser._id,
+    });
+    console.log(procedure);
+
+    const attendance = await Attendance.create({
+      healingPlan: healingPlan.id,
+      medic: testUser.id,
+      attendanceDate: new Date().toISOString(),
+      procedures: [
+        {
+          procedure: procedure.id,
+          procedureDynamic: 1,
+        },
+      ],
+      patientDynamic: 1,
+      beforeAttendance: {
+        comments: '',
+        pain: 5,
+      },
+      afterAttendance: {
+        comments: '',
+        pain: 5,
+      },
+    });
+    console.log(attendance);
+  } catch (error) {
+    db.close();
+    console.log(error);
+  }
 
   db.close();
 });
