f49ac0c294313c2f7cf4c67b4362acf152e63c6e
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-05-23 13:13:08 +0600

    Отрефакторил контроллер Первичного приёма

--- rekinetix-backend/controllers/primaryAssessment/primaryAssessment.controller.js
+++ rekinetix-backend/controllers/primaryAssessment/primaryAssessment.controller.js
@@ -1,36 +1,15 @@
 const PrimaryAssessment = require('../../models/PrimaryAssessment/PrimaryAssessment');
 const ObjectiveExam = require('../../models/PrimaryAssessment/ObjectiveExam');
-
-const getAreaDescriptionData = (data) => ({
-  D: data.D || '',
-  S: data.S || '',
-  additionalInfo: data.additionalInfo || '',
-});
-
-const getObjectiveExamData = (data) => ({
-  foot: getAreaDescriptionData(data.foot),
-  hip: getAreaDescriptionData(data.hip),
-  pelvicSpine: getAreaDescriptionData(data.pelvicSpine),
-  lumbarSpine: getAreaDescriptionData(data.lumbarSpine),
-  thoracicSpine: getAreaDescriptionData(data.thoracicSpine),
-  shoulderGirdel: getAreaDescriptionData(data.shoulderGirdel),
-  headAndNeck: getAreaDescriptionData(data.headAndNeck),
-});
-
-const getPrimaryAssessmentData = (data, userId, objectiveExamId) => ({
-  patient: data.patient,
-  attendingDoctor: userId,
-  assessmentDate: data.attendanceDate,
-  complaints: data.complaints,
-  anamnesisVitae: data.anamnesisVitae,
-  anamnesisMorbi: data.anamnesisMorbi,
-  objectiveExam: objectiveExamId,
-  examinations: data.examinations,
-  diagnosis: data.diagnosis,
-});
+const {
+  getObjectiveExamData,
+  getPrimaryAssessmentData,
+} = require('./utilities');
 
 const create = async (req, res) => {
-  const { objectiveExam } = req.body;
+  const { body, userId } = req;
+  const { objectiveExam } = body;
+
+  // TODO: Добавить валидацию.
   if (!(objectiveExam && objectiveExam.constructor === Object)) {
     res.sendStatus(400);
   }
@@ -40,8 +19,8 @@ const create = async (req, res) => {
   try {
     const objectiveExamDoc = await ObjectiveExam.create(objectiveExamData);
     const primaryAssessmentData = getPrimaryAssessmentData(
-      req.body,
-      req.userId,
+      body,
+      userId,
       objectiveExamDoc.id,
     );
     const primaryAssessmentDoc = await PrimaryAssessment.create(
