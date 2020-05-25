dfeb7556fb6d83c048e5eb7057bc9f530f75e4d9
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-25 15:49:17 +0600

    #89 Добавил условие на мок базы которое опирается на переменную окружения

--- rekinetix-backend/server.js
+++ rekinetix-backend/server.js
@@ -21,7 +21,17 @@ const connectToDb = () => {
   return new Promise((resolve, reject) => {
     
     if(process.env.NODE_ENV === 'test') {
-    
+      const Mockgoose = require('mockgoose').Mockgoose;
+      const mockgoose = new Mockgoose(mongoose);
+  
+      mockgoose.prepareStorage()
+        .then(() => {
+          mongoose.connect(config.db.getDbPath(), {useNewUrlParser: true})
+            .then((res, err) => {
+              if (err) return reject(err);
+              resolve();
+            });
+        });
     } else {
       mongoose.connect(config.db.getDbPath(), {useNewUrlParser: true})
         .then((res, err) => {
