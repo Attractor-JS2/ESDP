ce605b12566f2d3e0e2604f4d14914c3a7a17f31
Northern Stormy <gerrardthebanana@gmail.com>
2020-05-29 23:54:27 +0600

    #89 Исправил дефект при интеграции
    В результате пересборки ветки test-auth возникла ошибка, исравил

--- rekinetix-backend/controllers/user/user.js
+++ rekinetix-backend/controllers/user/user.js
@@ -1,18 +1,18 @@
 const User = require('../../models/User');
 
 const getResponseSafeData = (data) => ({
-    token: data.token,
-    fullname: data.fullname,
-    role: data.role,
+  token: data.token,
+  fullname: data.fullname,
+  role: data.role,
 });
 
 const createUser = async (req, res) => {
-    const user = new User({
-        fullname: req.body.fullname,
-        username: req.body.username,
-        role: req.body.role,
-        password: req.body.password,
-    });
+  const user = new User({
+    fullname: req.body.fullname,
+    username: req.body.username,
+    role: req.body.role,
+    password: req.body.password,
+  });
 
   try {
     await user.save();
@@ -22,41 +22,41 @@ const createUser = async (req, res) => {
   }
 };
 
-    const signIn = async (req, res) => {
-        const user = await User.findOne({username: req.body.username});
-        if (!user) {
-            return res.status(400).send({error: 'Wrong login or password'});
-        }
+const signIn = async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    return res.status(400).send({ error: 'Wrong login or password' });
+  }
 
-        const isMatch = await user.checkPassword(req.body.password);
-        if (!isMatch) {
-            return res.status(400).send({error: 'Wrong login or password'});
-        }
+  const isMatch = await user.checkPassword(req.body.password);
+  if (!isMatch) {
+    return res.status(400).send({ error: 'Wrong login or password' });
+  }
 
-        user.generateToken();
-        const savedData = await user.save();
+  user.generateToken();
+  const savedData = await user.save();
 
-        return res.status(200).send(getResponseSafeData(savedData));
-    };
+  return res.status(200).send(getResponseSafeData(savedData));
+};
 
-    const signOut = async (req, res) => {
-        const token = req.get('x-access-token');
-        const success = {message: 'Success'};
-        if (!token) return res.send(success);
+const signOut = async (req, res) => {
+  const token = req.get('x-access-token');
+  const success = { message: 'Success' };
+  if (!token) return res.send(success);
 
-        const user = await User.findOne({token});
-        if (!user) return res.send(success);
+  const user = await User.findOne({ token });
+  if (!user) return res.send(success);
 
-        user.generateToken();
-        await user.save();
+  user.generateToken();
+  await user.save();
 
-        return res.send(success);
-    };
+  return res.send(success);
+};
 
-    const userController = {
+const userController = {
   createUser,
   signIn,
   signOut,
 };
 
-module.exports = createRouter;
+module.exports = userController;
