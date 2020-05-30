84592a45cd48d6db20e36a143231bb9001b504d7
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-05-30 22:53:27 +0600

    Обновил экшен для сохранения процедуры в план

--- rekinetix-client/src/store/actions/healingPlan.js
+++ rekinetix-client/src/store/actions/healingPlan.js
@@ -1,12 +1,15 @@
-import {push} from "connected-react-router";
-import {NotificationManager} from "react-notifications";
+import { push } from 'connected-react-router';
+import { NotificationManager } from 'react-notifications';
 
 import axios from '../../axiosBackendInstance';
 import {
   FETCH_HEALING_PLAN_SUCCESS,
+  FETCH_HEALING_PLAN_FAILURE,
   ADD_PROCEDURE_TO_PLAN_SUCCESS,
+  ADD_PROCEDURE_TO_PLAN_FAILURE,
   REMOVE_PROCEDURE_FROM_PLAN_SUCCESS,
-} from "./actionTypes";
+  REMOVE_PROCEDURE_FROM_PLAN_FAILURE,
+} from './actionTypes';
 
 export const submitForm = (form) => {
   return dispatch => {
@@ -24,33 +27,65 @@ const fetchPlanSuccess = (healingPlan) => ({
   payload: healingPlan,
 });
 
-export const fetchPlanByPrimaryAssessment = (primaryAssessmentId) => (dispatch) => {
-  axios.get(`/healing-plans?primaryAssessment=${primaryAssessmentId}`).then(
+const fetchPlanFailure = (error) => ({
+  type: FETCH_HEALING_PLAN_FAILURE,
+  payload: error,
+});
+
+export const fetchPlanByPrimaryAssessment = (assessmentId) => (dispatch) => {
+  axios.get(`/healing-plans?primaryAssessment=${assessmentId}`).then(
+    (res) => dispatch(fetchPlanSuccess(res.data)),
+    (error) => console.log(error),
+  );
+};
+
+export const fetchPlanById = (healingPlanId) => (dispatch) => {
+  axios.get(`/healing-plans/${healingPlanId}`).then(
     (res) => dispatch(fetchPlanSuccess(res.data)),
     (error) => console.log(error),
   );
 };
 
 const addProcedureSuccess = () => ({ type: ADD_PROCEDURE_TO_PLAN_SUCCESS });
+const addProcedureFailure = (error) => ({
+  type: ADD_PROCEDURE_TO_PLAN_FAILURE,
+  payload: error,
+});
 
-export const addProcedureToPlan = (procedureData) => (dispatch) => {
-  axios.patch('/healingPlan/procedure/', procedureData).then(
+export const addProcedureToPlan = (procedureData) => (dispatch, getState) => {
+  const { _id } = getState().healingPlan.healingPlan;
+  axios.patch(`/healing-plans/${_id}`, procedureData).then(
     () => {
       dispatch(addProcedureSuccess());
-      // dispatch(fetchHealingPlan());
+      dispatch(fetchPlanById(_id));
     },
-    (error) => console.log(error),
+    (error) => dispatch(addProcedureFailure(error)),
   );
 };
 
-const removeProcedureSuccess = () => ({ type: REMOVE_PROCEDURE_FROM_PLAN_SUCCESS });
+const removeProcedureSuccess = () => ({
+  type: REMOVE_PROCEDURE_FROM_PLAN_SUCCESS,
+});
 
-export const removeProcedureFromPlan = (procedureStage, procedureName) => (dispatch) => {
-  axios.delete(`/healingPlan/procedure?stage=${procedureStage}&procedureName=${procedureName}`).then(
-    () => {
-      dispatch(removeProcedureSuccess());
-      // dispatch(fetchHealingPlan());
-    },
-    (error) => console.log(error),
-  );
+const removeProcedureFailure = (error) => ({
+  type: REMOVE_PROCEDURE_FROM_PLAN_FAILURE,
+  payload: error,
+});
+
+export const removeProcedureFromPlan = (procedureStage, procedureName) => (
+  dispatch,
+  getState,
+) => {
+  const { _id } = getState().healingPlan.healingPlan;
+  axios
+    .delete(
+      `/healingPlan/procedure?stage=${procedureStage}&procedureName=${procedureName}`,
+    )
+    .then(
+      () => {
+        dispatch(removeProcedureSuccess());
+        dispatch(fetchPlanById(_id));
+      },
+      (error) => dispatch(removeProcedureFailure(error)),
+    );
 };
