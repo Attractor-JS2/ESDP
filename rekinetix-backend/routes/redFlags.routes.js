b0e2a33ee2a54045234fe9dff9553248a01009ab
Northern Stormy <gerrardthebanana@gmail.com>
2020-06-02 02:06:06 +0600

    #102 Убрал вызов метода в роуте, понял, что сделал лишнюю работу
    Только сейчас заметил, что в коллекции redFlags хранятся уже отмеченные красные флаги, привязанные к пациенту, а не массив возможнных значений, как я думал, пока не стал удалять

--- rekinetix-backend/routes/redFlags.routes.js
+++ rekinetix-backend/routes/redFlags.routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const redFlagsController = require('../controllers/redFlags/redFlags.controller');
 
-router.get('/', redFlagsController.getRedFlags());
+router.get('/', redFlagsController.getRedFlags);
 
 module.exports = router;
