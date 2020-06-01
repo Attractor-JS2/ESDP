97f33e607c38d4a7ede451a5c8395efd283f8ffe
yelkanat <52521184+yelkanat@users.noreply.github.com>
2020-06-01 07:18:58 +0600

    добавил action для get post get/id запросов

--- rekinetix-client/src/store/actions/patientCards.js
+++ rekinetix-client/src/store/actions/patientCards.js
@@ -1,18 +1,34 @@
 import {push} from "connected-react-router";
 import axios from '../../axiosBackendInstance';
-import {FETCH_PATIENT_CARDS_SUCCESS} from "./actionTypes";
+import {FETCH_PATIENT_CARDS_SUCCESS, SEND_PATIENT_CARDS_SUCCESS,FETCH_PATIENT_INFO_SUCCESS} from "./actionTypes";
 import {NotificationManager} from "react-notifications";
 
-const fetchPatientCardsSuccess = cards => {
-  return {type: FETCH_PATIENT_CARDS_SUCCESS, cards}
+const fetchPatientCardsSuccess = patientCards => {
+  return {type: FETCH_PATIENT_CARDS_SUCCESS, patientCards}
+};
+const postsPatientCardsSuccess = () => {
+  return {type: SEND_PATIENT_CARDS_SUCCESS}
+};
+const fetchPatientInfoSuccess = patient =>{
+  return {type: FETCH_PATIENT_INFO_SUCCESS, patient}
+};
+export const postsPatientCards = (patientData) => (dispatch) => {
+  console.log(patientData)
+    axios.post('/patients',patientData).
+    then((res) => {
+      console.log(res.data)
+      dispatch(postsPatientCardsSuccess());
+      NotificationManager.success('Форма успешно отправлена');
+      dispatch(push('/patients'));
+    })
 };
 
 export const fetchPatientCards = () => {
   return (dispatch) => {
-    axios.get('/patientCards')
+    axios.get('/patients/')
       .then(res => {
-        const cards = res.data;
-        dispatch({type: FETCH_PATIENT_CARDS_SUCCESS, cards});
+        console.log(res.data, "res")
+        dispatch(fetchPatientCardsSuccess(res.data))
       })
   };
 };
