458eff4dccfdbcc68d1db45f957b4a7164db9a5c
Northern Stormy <gerrardthebanana@gmail.com>
2020-06-02 02:15:51 +0600

    #102 Переделал роутер и контроллер типов красных флагов

--- rekinetix-backend/routes/redFlags.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const redFlagsController = require('../controllers/redFlags/redFlags.controller');
-
-router.get('/', redFlagsController.getRedFlags);
-
-module.exports = router;
