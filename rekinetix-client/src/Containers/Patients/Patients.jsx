feb4b07e3a20af682c66a310b4e58d1dddf5ba72
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-06-01 11:20:07 +0600

    Реорганизовал импорты в компоненте Patients

--- rekinetix-client/src/Containers/Patients/Patients.jsx
+++ rekinetix-client/src/Containers/Patients/Patients.jsx
@@ -1,20 +1,24 @@
-import React, {useEffect} from "react";
-import PatientCard from "../../components/PatientCard/PatientCard";
-import {makeStyles} from "@material-ui/core/styles";
-import defaultImage from "../../assets/defaultImage/default-photo.jpeg";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Typography from "@material-ui/core/Typography";
-import CardMedia from "@material-ui/core/CardMedia";
-import {NavLink as RouterNavLink} from "react-router-dom";
-import {fetchPatientCards, fetchPatientInfo} from "../../store/actions/patientCards";
-import {connect} from "react-redux";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { NavLink as RouterNavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
+
+import PatientCard from '../../components/PatientCard/PatientCard';
+import defaultImage from '../../assets/defaultImage/default-photo.jpeg';
+import {
+  fetchAllPatients,
+  fetchPatientInfo,
+} from '../../store/actions/patients';
 
 function getModalStyle() {
   const top = 50;
   const left = 50;
-  
+
   return {
     top: `${top}%`,
     left: `${left}%`,
@@ -24,24 +28,24 @@ function getModalStyle() {
 
 const useStyles = makeStyles(() => ({
   paper: {
-    position: "absolute",
+    position: 'absolute',
   },
   media: {
     width: 200,
   },
   root: {
     width: 500,
-    display: "flex",
-    textAlign: "left",
+    display: 'flex',
+    textAlign: 'left',
   },
   title: {
     fontSize: 20,
-    borderBottom: '1px solid black'
+    borderBottom: '1px solid black',
   },
 }));
 
 function Patients(props) {
-  const {patient} = props;
+  const { patient } = props;
   const classes = useStyles();
   const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
@@ -50,21 +54,19 @@ function Patients(props) {
   const handleOpen = (id) => {
     setOpen(true);
     setPatient(id);
-    console.log(id);
   };
 
   useEffect(() => {
-    props.submitForm()
+    props.submitForm();
   }, []);
 
   const handleClose = () => {
     setOpen(false);
   };
   const handleFetchPatient = () => {
-    if(selectedPatient) {
-      props.onFetchPatientInfo(selectedPatient());
+    if (selectedPatient) {
+      props.onFetchPatientInfo(selectedPatient);
     }
-    console.log(id)
   };
   return (
     <div>
@@ -80,74 +82,74 @@ function Patients(props) {
           />
         );
       })}
-      <Modal 
+      <Modal
         open={open}
-        onEnter={()=>handleFetchPatient()}
+        onEnter={() => handleFetchPatient()}
         onClose={handleClose}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description"
       >
- <div style={modalStyle}  className={classes.paper}>
-      {patient && patient.patient ? (
-        <Card className={classes.root}>
-          <CardMedia
-            className={classes.media}
-            image={defaultImage}
-            title="Paella dish"
-          />    
-          <CardContent>
-            <Typography className={classes.title}>
-              Имя: {patient.patient.fullname}
-            </Typography>
-            <Typography className={classes.title}>
-              Год рождения: {new Date(patient.patient.birthday).toLocaleDateString()}
-            </Typography>
-            <Typography className={classes.title}>
-              Пол: {patient.patient.gender}
-            </Typography>
-            <Typography className={classes.title}>
-              Красные флаги: {patient.redFlags && patient.redFlags}
-            </Typography>
-            <Typography className={classes.title}>
-              Диагноз: {patient.diagnosis && patient.diagnosis}
-            </Typography>
-            <Typography>
-              <RouterNavLink to="/plan" exact>
-                План лечения
-              </RouterNavLink>
-            </Typography>
-            <Typography>
-              <RouterNavLink to="/plan-chart" exact>
-                Таблица плана лечения
-              </RouterNavLink>
-            </Typography>
-            <Typography>
-              <RouterNavLink to="/" exact>
-                Протокол первичного приема
-              </RouterNavLink>
-            </Typography>
-          </CardContent>
-        </Card>
-      ) : null}
-    </div>    
+        <div style={modalStyle} className={classes.paper}>
+          {patient && patient.patient ? (
+            <Card className={classes.root}>
+              <CardMedia
+                className={classes.media}
+                image={defaultImage}
+                title="Paella dish"
+              />
+              <CardContent>
+                <Typography className={classes.title}>
+                  Имя: {patient.patient.fullname}
+                </Typography>
+                <Typography className={classes.title}>
+                  Год рождения:{' '}
+                  {new Date(patient.patient.birthday).toLocaleDateString()}
+                </Typography>
+                <Typography className={classes.title}>
+                  Пол: {patient.patient.gender}
+                </Typography>
+                <Typography className={classes.title}>
+                  Красные флаги: {patient.redFlags && patient.redFlags}
+                </Typography>
+                <Typography className={classes.title}>
+                  Диагноз: {patient.diagnosis && patient.diagnosis}
+                </Typography>
+                <Typography>
+                  <RouterNavLink to="/plan" exact>
+                    План лечения
+                  </RouterNavLink>
+                </Typography>
+                <Typography>
+                  <RouterNavLink to="/plan-chart" exact>
+                    Таблица плана лечения
+                  </RouterNavLink>
+                </Typography>
+                <Typography>
+                  <RouterNavLink to="/" exact>
+                    Протокол первичного приема
+                  </RouterNavLink>
+                </Typography>
+              </CardContent>
+            </Card>
+          ) : null}
+        </div>
       </Modal>
-
     </div>
   );
 }
 
 const mapStateToProps = (state) => {
   return {
-    patientCards: state.patientCards.patientCards,
-    patient: state.patientCards.patient
-  }
+    patients: state.patients.patients,
+    currentPatient: state.patients.currentPatient,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    submitForm: (data) => dispatch(fetchPatientCards(data)),
-    onFetchPatientInfo: (id) => dispatch(fetchPatientInfo(id))
-  }
+    onFetchAllPatients: () => dispatch(fetchAllPatients()),
+    onFetchPatientInfo: (patientId) => dispatch(fetchPatientInfo(patientId)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Patients);
