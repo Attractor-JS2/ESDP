40f8b95a4749e3b896e885ecfaedd9eb5c3ca813
yelkanat <52521184+yelkanat@users.noreply.github.com>
2020-06-01 07:15:32 +0600

    изменил rout patient на patients и импортировал PatientRecord

--- rekinetix-client/src/Routes.js
+++ rekinetix-client/src/Routes.js
@@ -9,13 +9,13 @@ import {
   medic,
   diagnosis,
 } from "./components/HealingPlanChart/healingPlanDataLatest";
-import Patient from "./Containers/Patient/Patient";
-import PatientCardCreatingForm from "./Containers/PatientCardCreatingForm/PatientCardCreatingForm";
+import Patients from "./Containers/Patients/Patients";
 import PrimaryAssessment from "./Containers/PrimaryAssessment/PrimaryAssessment";
 import Login from "./Containers/Login/Login";
 import Register from "./Containers/Register/Register";
 import SiteMap from "./Containers/SiteMap/SiteMap";
 import { connect } from "react-redux";
+import PatientRecord from "./Containers/PatientRecord/PatientRecord";
 
 const ProtectedRoute = ({ isAllowed, ...props }) => (
   isAllowed ? <Route {...props} /> : <Redirect to="/" />
@@ -25,10 +25,10 @@ const Routes = ({ user }) => {
   return (
     <Switch>
       <Route path="/primary-assessment" component={PrimaryAssessment} />
-      <Route path="/patients" component={Patient} />
+      <Route path="/patients" component={Patients} />
       <Route path="/attendance" component={Attendance} />
       <Route path="/plan" component={HealingPlan} />
-      <Route path="/createPatient" component={PatientCardCreatingForm} />
+      <Route path="/createPatient" component={PatientRecord} />
       <Route path="/plan-chart">
         <HealingPlanChart
           redFlags={redFlags}
