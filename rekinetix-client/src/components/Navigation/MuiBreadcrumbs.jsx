0c38cdcbe22ec0a92f33d3e9e0b2fe93142f4055
Yerzhan Utelbayev <yerzhan.utelbayev@gmail.com>
2020-06-01 10:49:04 +0600

    Создал компонент для Breadcrumbs

---
+++ rekinetix-client/src/components/Navigation/MuiBreadcrumbs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+
+const MuiBreadcrumbs = () => {
+  return (
+    <Breadcrumbs>
+      
+    </Breadcrumbs>
+  )
+}
+
+export default MuiBreadcrumbs;
