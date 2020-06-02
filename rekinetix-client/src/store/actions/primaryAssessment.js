e30c5246e571f5b8254a423dbe95f475310bd62a
Northern Stormy <gerrardthebanana@gmail.com>
2020-06-02 17:50:20 +0600

    #102 Создал экшн на получение всех видов красных флагов

--- rekinetix-client/src/store/actions/primaryAssessment.js
+++ rekinetix-client/src/store/actions/primaryAssessment.js
@@ -1,6 +1,16 @@
 import axios from '../../axiosBackendInstance';
 import {NotificationManager} from "react-notifications";
 
+export const getRedFlags = () => {
+  return dispatch => {
+    axios.get('/suggestions/red-flag-types')
+      .then(res => {
+        console.log(res.data);
+      })
+  }
+};
+
+
 export const submitPrimaryAttendance = data => {
   return dispatch => {
     axios.post('/primary-assessments', data)
