3b4df30f31e23f1df402b4ac2a78878abd3385e6
Northern Stormy <gerrardthebanana@gmail.com>
2020-06-08 01:12:39 +0600

    #114 Изменил стили для карт пациентов
    Удалил заглушки для аватаров, вывел несколько карт в строку, изменил цветовую гамму

--- rekinetix-client/src/Containers/Patients/Patients.jsx
+++ rekinetix-client/src/Containers/Patients/Patients.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { Link as RouterLink } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
+import {Link as RouterLink} from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -13,26 +13,27 @@ import {
   resetPatientInfo,
 } from '../../store/actions/patients';
 import PatientFullInfo from './PatientFullInfo/PatientFullInfo';
+import {Button} from "@material-ui/core";
 
-const Patients = ({ patients, onFetchAllPatients, onResetCurrentPatient }) => {
+const Patients = ({patients, onFetchAllPatients, onResetCurrentPatient}) => {
   const [open, setOpen] = useState(false);
   const [selectedPatientId, setPatient] = useState(null);
-
+  
   const handleOpen = (id) => {
     setOpen(true);
     setPatient(id);
   };
-
+  
   useEffect(() => {
     onFetchAllPatients();
   }, [onFetchAllPatients]);
-
+  
   const handleClose = () => {
     setOpen(false);
     setPatient(null);
     onResetCurrentPatient();
   };
-
+  
   return (
     <Container>
       <PatientFullInfo
@@ -40,9 +41,9 @@ const Patients = ({ patients, onFetchAllPatients, onResetCurrentPatient }) => {
         handleClose={handleClose}
         patientId={selectedPatientId}
       />
-
-      <Paper>
-        <Grid container spacing={2} direction="row" justify="space-between">
+      
+      <Paper style={{marginBottom: '20px'}}>
+        <Grid container spacing={2} direction="row" justify="space-between" style={{padding: '20px'}}>
           <Grid item>
             <Typography variant="h4" component="h1">
               Пациенты
@@ -50,13 +51,14 @@ const Patients = ({ patients, onFetchAllPatients, onResetCurrentPatient }) => {
           </Grid>
           <Grid item>
             <Link component={RouterLink} to="/patients/new">
-              <Typography>Зарегистрировать нового пациента</Typography>
+              <Typography color="secondary">Зарегистрировать нового пациента</Typography>
             </Link>
           </Grid>
         </Grid>
       </Paper>
-      {patients && Array.isArray(patients) && patients.length > 0
-        ? patients.map((person) => {
+      <div className='d-flex justify-content-between flex-wrap'>
+        {patients && Array.isArray(patients) && patients.length > 0
+          ? patients.map((person) => {
             return (
               <PatientCard
                 key={person._id}
@@ -67,7 +69,9 @@ const Patients = ({ patients, onFetchAllPatients, onResetCurrentPatient }) => {
               />
             );
           })
-        : null}
+          : null}
+      </div>
+    
     </Container>
   );
 };
